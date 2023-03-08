ce48ae50da3ef59576b957b5ccdad4921cf24571
hilariohenrique <hilariohenr@hotmail.com>
2023-03-08 01:13:58 -0300

    First Commit

---
+++ src/main/java/br/com/ada/locatecarprojectjavawebI/LocateCarProjectJavaWebIApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.locatecarprojectjavawebI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocateCarProjectJavaWebIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocateCarProjectJavaWebIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/locatecarprojectjavawebI/LocateCarProjectJavaWebIApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.locatecarprojectjavawebI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocateCarProjectJavaWebIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
